b40b780e3486a4ed22c88c220f24465bc3641ac9
hhy <huanghaoyuanhhy@gmail.com>
2021-11-21 20:54:19 +0800

    reporter, proto: add batch write

--- src/reporter.rs
+++ src/reporter.rs
@@ -7,14 +7,19 @@ use std::str::FromStr;
 use std::time::Duration;
 use tokio::sync::mpsc;
 use tokio::time;
+use tokio::task;
+use tokio::time::MissedTickBehavior;
 use tonic::codegen::http::uri::InvalidUri;
 use tonic::transport::{Channel, Uri};
 use tracing::{info, warn};
 
 const RETRY_INTERVAL: u64 = 10;
+const BATCH_SIZE: usize = 1024;
+const BATCH_INTERVAL: Duration = Duration::from_secs(1);
 
 type PingResultRx = mpsc::Receiver<PingResult>;
 type TcpPingResultRx = mpsc::Receiver<TcpPingResult>;
+type FlushSignalTx = mpsc::Sender<()>;
 
 #[derive(Clone)]
 pub struct Reporter {
@@ -29,11 +34,11 @@ impl Reporter {
         Ok(Self { channel, agent_id })
     }
 
-    fn build_ping_request(&self, result: PingResult) -> PingReportReq {
-        let result = result.into();
+    fn build_ping_request(&self, results: Vec<PingResult>) -> PingReportReq {
+        let r = results.into_iter().map(|x|x.into()).collect();
         PingReportReq {
             agent_id: self.agent_id,
-            result: Some(result),
+            results: r,
         }
     }
 
@@ -54,9 +59,25 @@ impl Reporter {
         time::sleep(wait).await;
     }
 
+    fn start_timer(period: Duration, tx: FlushSignalTx) {
+        let mut timer = time::interval(period);
+        timer.set_missed_tick_behavior(MissedTickBehavior::Delay);
+
+        task::spawn(async move {
+            loop {
+                timer.tick().await;
+                tx.send(()).await.expect("Send flush buff signal fail");
+            }
+        });
+    }
+
     pub(crate) async fn report_ping_result(self, mut rx: PingResultRx) {
         let mut client = CollectorClient::new(self.channel.clone());
         let (failed_tx, mut failed_rx) = mpsc::channel::<PingReportReq>(1);
+        let (flush_buff_tx, mut flush_buff_rx) = mpsc::channel(1);
+        let mut buff = Vec::with_capacity(BATCH_SIZE);
+
+        Self::start_timer(BATCH_INTERVAL, flush_buff_tx.clone());
 
         loop {
             tokio::select! {
@@ -71,13 +92,21 @@ impl Reporter {
                         Self::backoff().await;
                     }
                 }
-                r = rx.recv() => {
-                    let r = r.expect("Recv ping result fail");
-                    let req = self.build_ping_request(r);
+                s = flush_buff_rx.recv() => {
+                    s.expect("Recv flush buff signal fail");
+                    let req = self.build_ping_request(buff);
                     let result = client.ping_report(req.clone()).await;
                     if let Err(e) = result {
                         warn!("Send ping result fail, err:{}", e.message());
                         failed_tx.send(req).await.expect("Secv failed req fail");
+                    }
+                    buff = Vec::with_capacity(BATCH_SIZE);
+                }
+                r = rx.recv() => {
+                    let r = r.expect("Recv ping result fail");
+                    buff.push(r);
+                    if buff.len() == BATCH_SIZE {
+                        flush_buff_tx.send(()).await.expect("Send flush buff signal fail")
                     }
                 }
             }
